5c77838e51515194a4b35de252c8b469c0406cdb
Samuel Gomez <samuel.gomez@axa.fr>
2018-11-19 09:13:52 +0100

    fix build

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import Redbox from "redbox-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Redbox from 'redbox-react';
 
-import Presentation from "./presentation";
-
-const CustomErrorReporter = ({ error }) => <Redbox error={ error } />;
+const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
 
 CustomErrorReporter.propTypes = {
   error: PropTypes.instanceOf(Error).isRequired
@@ -14,18 +12,19 @@ CustomErrorReporter.propTypes = {
 
 ReactDOM.render(
   <AppContainer errorReporter={CustomErrorReporter}>
-    <Presentation />
+    <p>Hello</p>
   </AppContainer>,
-  document.getElementById("root"),
+  document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept("./presentation", () => {
-    const NextPresentation = require("./presentation").default;    ReactDOM.render(
+  module.hot.accept('./presentation', () => {
+    const NextPresentation = require('./presentation').default;
+    ReactDOM.render(
       <AppContainer errorReporter={CustomErrorReporter}>
         <NextPresentation />
       </AppContainer>,
-      document.getElementById("root"),
+      document.getElementById('root')
     );
   });
 }
--- presentation/index.js
+++ presentation/index.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 
 import {
-  Anim,
   Appear,
-  BlockQuote,
-  Cite,
   CodePane,
-  ComponentPlayground,
   Deck,
   Fill,
   Heading,
@@ -15,32 +11,17 @@ import {
   Link,
   ListItem,
   List,
-  Markdown,
-  MarkdownSlides,
-  Quote,
-  Slide,
-  SlideSet,
-  TableBody,
-  TableHeader,
-  TableHeaderItem,
-  TableItem,
-  TableRow,
-  Table,
-  Text,
-  GoToAction,
+  Slide
 } from 'spectacle';
-import CodeSlide from 'spectacle-code-slide';
 import createTheme from 'spectacle/lib/themes/default';
 
-//require('normalize.css');
 import 'normalize.css/normalize.css';
-import city from '../assets/images/city.jpg';
 
 const theme = createTheme({
   primary: '#61dafb',
   third: '#49d295',
   fourth: '#FF637E',
-  other: '#BB84FF',
+  other: '#BB84FF'
 });
 
 import Speaker from './Speaker';
@@ -75,265 +56,205 @@ import notetoolkit from '../assets/notes/notetoolkit';
 import notelerna from '../assets/notes/notelerna';
 import notelivecode from '../assets/notes/notelivecode';
 
-export default class Presentation extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      steps: 0,
-    };
-    this.updateSteps = this.updateSteps.bind(this);
-  }
-
-  updateSteps(steps) {
-    if (this.state.steps !== steps) {
-      this.setState({ steps });
-    }
-  }
-
-  render() {
-    return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
-        <Speaker
-          name="Samuel Gomez"
-          job="Software Engineer Senior"
-          company="Axa"
-          avatar={sam}
-          website="https://www.samuelgomez.fr"
-          companyIcon={axa}
-          bg={sambg}
-          twitter="gamuez"
-          github="samuel-gomez"
-        />
-        <Speaker
-          name="Guillaume Chervet"
-          job="Software Engineer Senior"
-          company="Axa"
-          avatar={gui}
-          website="https://www.guillaume-chervet.fr/"
-          companyIcon={axa}
-          bg={guibg}
-          twitter="guichervet"
-          github="guillaumechervet"
-        />
-        <Slide
-          transition={['zoom', 'fade']}
-          bgColor="other"
-          notes={notetoolkit}
-        >
-          <Heading size={1} caps lineHeight={1} textColor="black">
-            Retro
-          </Heading>
-          <Layout>
-            <Appear fid="1">
-              <Fill className="retro__fill">
-                <Image className="retro__img" src={jquery} width={60} />
-                <Image className="retro__img" width={60} src={css3} />
-                <Image className="retro__img" width={60} src={bootstrap} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Jquery / CSS / Bootstrap
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="2">
-              <Fill className="retro__fill">
-                <Image className="retro__img" src={angular} width={60} />
-                <Image className="retro__img" width={60} src={css3} />
-                <Image className="retro__img" width={60} src={bootstrap} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Angularjs / CSS / Bootstrap
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="3">
-              <Fill className="retro__fill retro__fill--actual">
-                <div className="retro__fill-part">
-                  <Image className="retro__img" src={react} width={60} />
-                  <Image className="retro__img" src={question} width={60} />
-                  <Heading
-                    size={6}
-                    textColor="secondary"
-                    bgColor="white"
-                    margin={10}
-                  >
-                    React / ?
-                  </Heading>
-                </div>
-                <div className="retro__fill-part">
-                  <Image className="retro__img" width={60} src={sass} />
-                  <Image className="retro__img" width={60} src={bem} />
-                  <Heading size={6} textColor="secondary" margin={10}>
-                    HTML / SASS / BEM
-                  </Heading>
-                </div>
-              </Fill>
-            </Appear>
-          </Layout>
-        </Slide>
-
-        <Slide
-          transition={['zoom', 'fade']}
-          bgColor="primary"
-          notes={notedesign}
-        >
-          <Heading size={1} caps lineHeight={1} textColor="black">
-            Design System
-          </Heading>
-          <Heading size={4} caps lineHeight={1} textColor="white">
-            avec l'Atomic Design
-          </Heading>
-          <Layout>
-            <Appear fid="1">
-              <Fill>
-                <Image src={atom} height={130} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Atoms
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="2">
-              <Fill>
-                <Image src={molecules} height={130} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Molecules
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="3">
-              <Fill>
-                <Image src={organism} height={130} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Organism
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="4">
-              <Fill>
-                <Image src={template} height={130} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                >
-                  Templates
-                </Heading>
-              </Fill>
-            </Appear>
-            <Appear fid="5">
-              <Fill>
-                <Image src={pages} height={130} />
-                <Heading
-                  size={6}
-                  textColor="secondary"
-                  bgColor="white"
-                  margin={10}
-                  height
-                >
-                  Pages
-                </Heading>
-              </Fill>
-            </Appear>
-          </Layout>
-        </Slide>
-
-        <Slide
-          transitionIn={['zoom', 'fade']}
-          transitionOut={['slide', 'fade']}
-          bgColor="fourth"
-          notes={notebem}
-        >
-          <Heading size={1} caps lineHeight={1} textColor="black">
-            BEM
-          </Heading>
-          <Heading size={4} caps lineHeight={1} textColor="white">
-            Block Element Modifier
-          </Heading>
-          <CodePane
-            lang="sass"
-            source={require('raw-loader!../assets/code/bem.style')}
-            margin="20px auto"
-            overflow="overflow"
-          />
-        </Slide>
-
-        <Slide transition={['zoom']} bgColor="primary" notes={notelerna}>
-          <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Lerna & NPM
-          </Heading>
-          <Heading size={1} fit caps>
-            Monorepo et packages
-          </Heading>
-          <Image src={lerna} />
-          <Image src={npm} />
-        </Slide>
-
-        <Slide transition={['spin']} bgColor="third" notes={notelivecode}>
-          <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Live Coding
-          </Heading>
-          <Heading size={1} fit caps>
-            Part 1 : Installation, mise en place et première page
-          </Heading>
-        </Slide>
-
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Open source !
-          </Heading>
-          <List>
-            <ListItem>
-              <Link
-                textColor="black"
-                href="https://www.npmjs.com/search?q=%40axa-fr%2Freact-toolkit"
+export default () => (
+  <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
+    <Speaker
+      name="Samuel Gomez"
+      job="Software Engineer Senior"
+      company="Axa"
+      avatar={sam}
+      website="https://www.samuelgomez.fr"
+      companyIcon={axa}
+      bg={sambg}
+      twitter="gamuez"
+      github="samuel-gomez"
+    />
+    <Speaker
+      name="Guillaume Chervet"
+      job="Software Engineer Senior"
+      company="Axa"
+      avatar={gui}
+      website="https://www.guillaume-chervet.fr/"
+      companyIcon={axa}
+      bg={guibg}
+      twitter="guichervet"
+      github="guillaumechervet"
+    />
+    <Slide transition={['zoom', 'fade']} bgColor="other" notes={notetoolkit}>
+      <Heading size={1} caps lineHeight={1} textColor="black">
+        Retro
+      </Heading>
+      <Layout>
+        <Appear fid="1">
+          <Fill className="retro__fill">
+            <Image className="retro__img" src={jquery} width={60} />
+            <Image className="retro__img" width={60} src={css3} />
+            <Image className="retro__img" width={60} src={bootstrap} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Jquery / CSS / Bootstrap
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="2">
+          <Fill className="retro__fill">
+            <Image className="retro__img" src={angular} width={60} />
+            <Image className="retro__img" width={60} src={css3} />
+            <Image className="retro__img" width={60} src={bootstrap} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Angularjs / CSS / Bootstrap
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="3">
+          <Fill className="retro__fill retro__fill--actual">
+            <div className="retro__fill-part">
+              <Image className="retro__img" src={react} width={60} />
+              <Image className="retro__img" src={question} width={60} />
+              <Heading
+                size={6}
+                textColor="secondary"
+                bgColor="white"
+                margin={10}
               >
-                Package NPM
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link
-                textColor="black"
-                href="https://github.com/AxaGuilDEv/react-toolkit"
-              >
-                Github
-              </Link>
-            </ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="white">
-          <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Merci !
-          </Heading>
-        </Slide>
-      </Deck>
-    );
-  }
-}
+                React / ?
+              </Heading>
+            </div>
+            <div className="retro__fill-part">
+              <Image className="retro__img" width={60} src={sass} />
+              <Image className="retro__img" width={60} src={bem} />
+              <Heading size={6} textColor="secondary" margin={10}>
+                HTML / SASS / BEM
+              </Heading>
+            </div>
+          </Fill>
+        </Appear>
+      </Layout>
+    </Slide>
+
+    <Slide transition={['zoom', 'fade']} bgColor="primary" notes={notedesign}>
+      <Heading size={1} caps lineHeight={1} textColor="black">
+        Design System
+      </Heading>
+      <Heading size={4} caps lineHeight={1} textColor="white">
+        avec l'Atomic Design
+      </Heading>
+      <Layout>
+        <Appear fid="1">
+          <Fill>
+            <Image src={atom} height={130} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Atoms
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="2">
+          <Fill>
+            <Image src={molecules} height={130} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Molecules
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="3">
+          <Fill>
+            <Image src={organism} height={130} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Organism
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="4">
+          <Fill>
+            <Image src={template} height={130} />
+            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
+              Templates
+            </Heading>
+          </Fill>
+        </Appear>
+        <Appear fid="5">
+          <Fill>
+            <Image src={pages} height={130} />
+            <Heading
+              size={6}
+              textColor="secondary"
+              bgColor="white"
+              margin={10}
+              height
+            >
+              Pages
+            </Heading>
+          </Fill>
+        </Appear>
+      </Layout>
+    </Slide>
+
+    <Slide
+      transitionIn={['zoom', 'fade']}
+      transitionOut={['slide', 'fade']}
+      bgColor="fourth"
+      notes={notebem}
+    >
+      <Heading size={1} caps lineHeight={1} textColor="black">
+        BEM
+      </Heading>
+      <Heading size={4} caps lineHeight={1} textColor="white">
+        Block Element Modifier
+      </Heading>
+      <CodePane
+        lang="sass"
+        source={require('raw-loader!../assets/code/bem.style')}
+        margin="20px auto"
+        overflow="overflow"
+      />
+    </Slide>
+
+    <Slide transition={['zoom']} bgColor="primary" notes={notelerna}>
+      <Heading size={1} fit caps lineHeight={1} textColor="black">
+        Lerna et NPM
+      </Heading>
+      <Heading size={1} fit caps>
+        Monorepo et packages
+      </Heading>
+      <Image src={lerna} />
+      <Image src={npm} />
+    </Slide>
+
+    <Slide transition={['spin']} bgColor="third" notes={notelivecode}>
+      <Heading size={1} fit caps lineHeight={1} textColor="black">
+        Live Coding
+      </Heading>
+      <Heading size={1} fit caps>
+        Part 1 : Installation, mise en place et première page
+      </Heading>
+    </Slide>
+
+    <Slide transition={['zoom']} bgColor="primary">
+      <Heading size={1} fit caps lineHeight={1} textColor="black">
+        Open source !
+      </Heading>
+      <List>
+        <ListItem>
+          <Link
+            textColor="black"
+            href="https://www.npmjs.com/search?q=%40axa-fr%2Freact-toolkit"
+          >
+            Package NPM
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link
+            textColor="black"
+            href="https://github.com/AxaGuilDEv/react-toolkit"
+          >
+            Github
+          </Link>
+        </ListItem>
+      </List>
+    </Slide>
+    <Slide transition={['fade']} bgColor="white">
+      <Heading size={1} fit caps lineHeight={1} textColor="black">
+        Merci !
+      </Heading>
+    </Slide>
+  </Deck>
+);
