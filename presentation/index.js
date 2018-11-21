b01b055f26de3165bbe085d1674ab56389c3e648
Samuel Gomez <samuel.gomez@axa.fr>
2018-11-22 00:08:50 +0100

    updates before talk

--- presentation/index.js
+++ presentation/index.js
@@ -41,6 +41,8 @@ import angular from '../assets/images/angularjs.svg';
 import bootstrap from '../assets/images/bootstrap.svg';
 import bem from '../assets/images/bem.svg';
 import question from '../assets/images/question.svg';
+import corecss from '../assets/images/corecss.png';
+import storybook from '../assets/images/storybook.png';
 
 import atom from '../assets/images/atoms.svg';
 import molecules from '../assets/images/molecules.svg';
@@ -59,9 +61,22 @@ import notemerci from '../assets/notes/notemerci';
 
 export default () => (
   <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
+    <Slide className="splash" transition={['zoom']} bgColor="primary">
+      <Heading size={1} fit caps lineHeight={1} textColor="black">
+        Tout savoir sur le Web Toolkit Axa
+      </Heading>
+      <Layout>
+        <Fill className="splash__part">
+          <Image className="splash__img" src={storybook} />
+        </Fill>
+        <Fill className="splash__part">
+          <Image className="splash__img" src={corecss} />
+        </Fill>
+      </Layout>
+    </Slide>
     <Speaker
       name="Samuel Gomez"
-      job="Software Engineer Senior"
+      job="Lead Front-end"
       company="Axa"
       avatar={sam}
       website="https://www.samuelgomez.fr"
@@ -72,7 +87,7 @@ export default () => (
     />
     <Speaker
       name="Guillaume Chervet"
-      job="Software Engineer Senior"
+      job="TechLeader"
       company="Axa"
       avatar={gui}
       website="https://www.guillaume-chervet.fr"
@@ -81,42 +96,81 @@ export default () => (
       twitter="guichervet"
       github="guillaumechervet"
     />
-    <Slide transition={['zoom', 'fade']} bgColor="other" notes={notetoolkit}>
-      <Heading size={1} caps lineHeight={1} textColor="black">
-        Retro
+    <Slide
+      className="retro"
+      transition={['zoom', 'fade']}
+      bgColor="other"
+      notes={notetoolkit}
+    >
+      <Heading caps size={2} margin={10}>
+        Retro Toolkit
       </Heading>
       <Layout>
         <Appear fid="1">
           <Fill className="retro__fill">
-            <Image className="retro__img" src={jquery} width={60} />
-            <Image className="retro__img" width={60} src={css3} />
-            <Image className="retro__img" width={60} src={bootstrap} />
-            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
-              Jquery / CSS / Bootstrap
+            <Heading
+              className="retro__title"
+              size={4}
+              textColor="secondary"
+              margin={10}
+            >
+              Toolkit V1
             </Heading>
+            <div className="retro__img-container">
+              <Image className="retro__img" src={jquery} width={60} />
+              <Image className="retro__img" width={60} src={css3} />
+              <Image className="retro__img" width={60} src={bootstrap} />
+              <Image className="retro__img" src={angular} width={60} />
+            </div>
+            <Heading size={6} textColor="secondary" margin={20}>
+              Jquery / CSS / Bootstrap / AngularJs
+            </Heading>
+            <List className="retro__list">
+              <ListItem>Gain de temps sur les projets</ListItem>
+              <ListItem>Homogénéisation graphiques et fonctionnelle</ListItem>
+              <ListItem>Code spaguetti</ListItem>
+            </List>
           </Fill>
         </Appear>
         <Appear fid="2">
           <Fill className="retro__fill">
-            <Image className="retro__img" src={angular} width={60} />
-            <Image className="retro__img" width={60} src={css3} />
-            <Image className="retro__img" width={60} src={bootstrap} />
-            <Heading size={6} textColor="secondary" bgColor="white" margin={10}>
-              Angularjs / SASS / Bootstrap / Form Generator
+            <Heading
+              className="retro__title"
+              size={4}
+              textColor="secondary"
+              margin={10}
+            >
+              Toolkit V2
             </Heading>
+            <div className="retro__img-container">
+              <Image className="retro__img" src={angular} width={60} />
+              <Image className="retro__img" width={60} src={css3} />
+              <Image className="retro__img" width={60} src={bootstrap} />
+            </div>
+            <Heading size={6} textColor="secondary" margin={10}>
+              Angularjs / SASS / Bootstrap
+            </Heading>
+            <List className="retro__list">
+              <ListItem>Fork de AngularStrap</ListItem>
+              <ListItem>Prémisses d'un design system</ListItem>
+              <ListItem>Form Generator</ListItem>
+            </List>
           </Fill>
         </Appear>
         <Appear fid="3">
           <Fill className="retro__fill retro__fill--actual">
+            <Heading
+              className="retro__title"
+              size={4}
+              textColor="secondary"
+              margin={10}
+            >
+              Toolkit V3
+            </Heading>
             <div className="retro__fill-part">
               <Image className="retro__img" src={react} width={60} />
               <Image className="retro__img" src={question} width={60} />
-              <Heading
-                size={6}
-                textColor="secondary"
-                bgColor="white"
-                margin={10}
-              >
+              <Heading size={6} textColor="secondary" margin={10}>
                 React / ?
               </Heading>
             </div>
@@ -127,6 +181,12 @@ export default () => (
                 HTML / SASS / BEM
               </Heading>
             </div>
+            <List className="retro__list">
+              <ListItem>Framework JS Agnostique</ListItem>
+              <ListItem>Séparation en 2 cores</ListItem>
+              <ListItem>Multipackages</ListItem>
+              <ListItem>Gain de 40%</ListItem>
+            </List>
           </Fill>
         </Appear>
       </Layout>
@@ -198,26 +258,65 @@ export default () => (
       <Heading size={1} caps lineHeight={1} textColor="black">
         BEM
       </Heading>
-      <Heading size={4} caps lineHeight={1} textColor="white">
-        Block Element Modifier
+      <Heading size={6} lineHeight={1} textColor="white">
+        .block__element--modifier
       </Heading>
-      <CodePane
-        lang="sass"
-        source={require('raw-loader!../assets/code/bem.style')}
-        margin="20px auto"
-        overflow="overflow"
-      />
+      <List>
+        <ListItem>
+          <b>C'est quoi</b> : C'est une convention de nommage
+        </ListItem>
+        <ListItem>
+          <b>Pourquoi</b> : le cascading amène à des selecteurs illisibles :
+          <Heading size={6} lineHeight={1} textColor="white">
+            div#wall .parent #tag-5.slick
+          </Heading>
+        </ListItem>
+        <ListItem>
+          <b>Pourquoi pas PostCSS </b> : Scope fort, rend difficile le
+          cascading, class CSS générée non lisibles.
+        </ListItem>
+        <ListItem>
+          <b>Avantages</b> :
+          <List>
+            <ListItem>
+              Assure la portabilité, ré-utilisation Isolation forte
+            </ListItem>
+            <ListItem>Permet de quand même du cascading</ListItem>
+            <ListItem>Classes CSS lisibles par l'humain </ListItem>
+          </List>
+        </ListItem>
+      </List>
     </Slide>
 
     <Slide transition={['zoom']} bgColor="primary" notes={notelerna}>
-      <Heading size={1} fit caps lineHeight={1} textColor="black">
+      <Heading size={3} caps lineHeight={1} textColor="black">
         Lerna et NPM
       </Heading>
-      <Heading size={1} fit caps>
+      <Heading size={4} caps textColor="white">
         Monorepo et packages
       </Heading>
-      <Image src={lerna} />
-      <Image src={npm} />
+      <Layout>
+        <Fill>
+          <Image src={lerna} />
+        </Fill>
+        <Fill>
+          <Image src={npm} width={300} />
+        </Fill>
+      </Layout>
+
+      <List>
+        <ListItem>
+          <b>C'est quoi</b> : Créer du multipackage en un seul repo
+        </ListItem>
+        <ListItem>
+          <b>Pourquoi</b> : Installation globale ou indépendante des packages,
+          bundle optimisé
+        </ListItem>
+        <ListItem>
+          <b>Avantages</b> : Maintenance et évolution des composants
+          indépendantes
+        </ListItem>
+      </List>
     </Slide>
 
     <Slide transition={['spin']} bgColor="third" notes={notelivecode}>
@@ -247,7 +346,12 @@ export default () => (
             textColor="black"
             href="https://github.com/AxaGuilDEv/react-toolkit"
           >
-            Github
+            Repo Github
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link textColor="black" href="https://axaguildev.github.io/">
+            Github AxaGuilDev
           </Link>
         </ListItem>
       </List>
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -96,7 +96,6 @@ module.exports = {
 
   optimization: {
     minimize: true,
-
     minimizer: [new UglifyJsPlugin()]
   }
 };
