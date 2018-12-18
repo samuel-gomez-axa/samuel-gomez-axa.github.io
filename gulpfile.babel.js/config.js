73ef72562a5be7aec24026115efa44287199671e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 16:47:55 +0000

    Deploy samuel-gomez-axa/samuel-gomez-axa.github.io to github.com/samuel-gomez-axa/samuel-gomez-axa.github.io.git:gh-pages

--- gulpfile.babel.js/config.js
+++ gulpfile.babel.js/config.js
@@ -2,6 +2,8 @@ export default {
   pathSrc: './src',
   pathDest: './dist',
   pathImg: '/assets/images',
+  pathImgDist: '/assets',
+  pathFavicons: '/assets/favicons',
   jsFiles: '/**/*.js',
   sassIndex: '/index.scss',
   sassFiles: '/**/*.scss',
@@ -10,5 +12,5 @@ export default {
   jsonFiles: '/data/*.json',
   pathSvg: '/assets/icons',
   svgFiles: '/*.svg',
-  pathSprite: '/assets/sprite'
+  pathSprite: '/assets/sprite',
 };
